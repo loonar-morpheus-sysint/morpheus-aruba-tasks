1b577ec12f19002df5ab77232bec97a249fbb6d0
Sandro Cicero <devopsvanilla@outlook.com>
2025-10-30 07:51:36 -0300

    refactor: create vrf

--- scripts/aruba/vrf/fabric-composer/wrapper-create-vrf-afc.sh
+++ scripts/aruba/vrf/fabric-composer/wrapper-create-vrf-afc.sh
@@ -66,11 +66,8 @@ AFC_API_JSON=$(cat <<EOF
 EOF
 )
 
-# Remove aspas simples ao redor do JSON, se existirem
-if [[ "$AFC_API_JSON" =~ ^".*"$ ]]; then
-    AFC_API_JSON="${AFC_API_JSON:1:-1}"
-fi
-if [[ "$AFC_API_JSON" =~ ^'.*'$ ]]; then
+## Remove aspas simples externas do JSON, se existirem
+if [[ "$AFC_API_JSON" =~ ^\'\{ ]]; then
     AFC_API_JSON="${AFC_API_JSON:1:-1}"
 fi
 echo "$AFC_API_JSON"
